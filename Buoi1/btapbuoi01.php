4a6d79e4c12dfbcf7dfbfcc9993cee2024a47eb0
Hoàng Thị Ngọc Ánh <ngoccanhh2206@gmail.com>
2026-08-12 13:07:18 +0700

    BTVN b1

---
+++ Buoi1/btapbuoi01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Chương trình PHP đầu tiên</title>
+</head>
+
+<body>
+    <?php
+    echo "<h1>Xin chào Hoàng Thị Ngọc Ánh!</h1>";
+    ?>
+</body>
+
+</html>
